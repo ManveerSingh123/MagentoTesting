9c33bbc9dce0ba7dd3ec13f268e5eb1fda847749
ABC FOR JAVA TESTING <ABC FOR JAVA TESTING@DESKTOP-A008EV8>
2020-01-28 07:34:34 +0000

    first commit

---
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -0,0 +1,34 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class MagentoTest
+{
+	@Test
+	public void test()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://magento.com");
+		Welcome w = new Welcome(driver);
+		w.clickOnMyAcoount();
+		Login l = new Login(driver);
+		l.typeEmail("kundansinghmahisour@gmail.com");
+		l.typePassword("Welcome@123");
+		l.clickOnLogin();
+		Home h = new Home(driver);
+		h.clickOnLogout();
+		driver.quit();
+		
+		
+		
+		}
+
+}
