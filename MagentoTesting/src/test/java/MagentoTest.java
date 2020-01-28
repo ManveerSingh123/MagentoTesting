32af58d231101a237bee4e3a89d66638eb5df0ec
ABC FOR JAVA TESTING <ABC FOR JAVA TESTING@DESKTOP-A008EV8>
2020-01-28 07:38:59 +0000

    second Commit

--- MagentoTesting/src/test/java/MagentoTest.java
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
@@ -13,7 +14,7 @@ public class MagentoTest
 	@Test
 	public void test()
 	{
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://magento.com");
